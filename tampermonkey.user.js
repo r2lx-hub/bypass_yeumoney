47f8219f83942851529f36a135b97a54d1356cf4
r2lx-hub <r2lxhub@gmail.com>
2025-07-18 17:35:31 +0700

    Add files via upload

--- tampermonkey.user.js
+++ tampermonkey.user.js
@@ -4,7 +4,7 @@
 // @version      18/07/2025
 // @description  Auto vượt yeumoney không cần chạm tay
 // @author       By R2lx
-// @require      https://raw.githubusercontent.com/r2lx-hub/bypass_yeumoney/refs/heads/main/decrypted_bypass_yeumoney.js
+// @require      https://raw.githubusercontent.com/r2lx-hub/bypass_yeumoney/refs/heads/main/bypass_link_yeumoney.js
 // @match        https://yeumoney.com/*
 // @exclude      https://yeumoney.com/quangly/*
 // @icon         
